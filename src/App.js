fc12116e2ade05dfd68a3164f7d6f2684973aa77
chetna-sewaiwar <chetnasewaiwar@gmail.com>
2023-06-12 00:47:14 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Navbar from './Navbar/Navbar';
+
+import './App.css';
+import RouterFile from './Components/RouterFile';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      {/* <Home/> */}
+      <RouterFile/>
+     
+    </div>
+  );
+}
+
+export default App;
